31e80b7137fdb2eb7d3317f59440a47cc598bb70
mitaka1959 <126474807+mitaka1959@users.noreply.github.com>
2024-10-25 17:43:34 +0300

    Update current.ts

--- pages/api/current.ts
+++ pages/api/current.ts
@@ -4,14 +4,14 @@ import serverAuth from "@/prisma/lib/serverauth";
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     // Allow only GET requests
     if (req.method !== 'GET') {
-        return res.status(405).end(); // Method not allowed
+        return res.status(405).end(); 
     }
 
     try {
-        const { currentUser } = await serverAuth(req); // Assuming serverAuth is properly defined
-        return res.status(200).json(currentUser); // Return the current user
+        const { currentUser } = await serverAuth(req); 
+        return res.status(200).json(currentUser); 
     } catch (error) {
-        console.error(error); // Log the error for debugging
-        return res.status(400).end(); // Bad request
+        console.error(error); 
+        return res.status(400).end(); 
     }
 }
